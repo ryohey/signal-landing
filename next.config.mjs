0dc1fb1aebcc60bb8b401cf3d43f5ab7986d3458
ryohey <info@codingcafe.jp>
2025-09-08 23:02:55 +0900

    Add basePath
    https://moldspoon.jp/blog/posts/deploy-blog-to-vercel-with-slash-blog-uri

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ import createNextIntlPlugin from "next-intl/plugin"
 const withNextIntl = createNextIntlPlugin()
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: "/landing",
+  assetPrefix: "/landing",
+  async rewrites() {
+    return [
+      {
+        source: "/landing/:path*",
+        destination: "/:path*",
+      },
+      {
+        source: "/landing/_next/:path*",
+        destination: "/_next/:path*",
+      },
+    ]
+  },
+}
 
 export default withNextIntl(nextConfig)
